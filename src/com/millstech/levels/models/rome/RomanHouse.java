6bb6ac89295ae06962eaf6d401e601916a943d96
RDMintzer <RDMintzer@gmail.com>
2017-06-04 18:45:32 -0700

    House Configs
    Finished house configurations, commented out until textures added.

--- src/com/millstech/levels/models/rome/RomanHouse.java
+++ src/com/millstech/levels/models/rome/RomanHouse.java
@@ -4,36 +4,37 @@ import com.millstech.textures.Textures;
 import com.millstech.toolbox.TileUtils;
 
 public class RomanHouse {
-	public static void loadHouse(int posX, int posY, int segmentNumber){
-	if(segmentNumber == 1)
+	public static void loadHouse(int posX, int posY, int version){
+	if(version == 1)
 	{
-		//TileUtils.createNoInteractionPlatform(columnLWithDoor, posX, posY);
-		//TileUtils.createPlatform(columnLeftTop, posX, posY + 1);
-		//TileUtils.createNoInteractionPlatform(roofTileLSmall, posX, posY + 2);
+		/*
+		TileUtils.createNoInteractionPlatform(columnLWithDoor, posX, posY);
+		TileUtils.createPlatform(columnLeftTop, posX, posY + 1);
+		TileUtils.createNoInteractionPlatform(roofTileLSmall, posX, posY + 2);
 		
-		//TileUtils.createNoInteractionPlatform(doorEntrance, posX + 1, posY);
-		//TileUtils.createNoInteractionPlatform(wallL, posX + 1, posY + 1);
-		//TileUtils.createPlatform(wallLCorner, posX + 1, posY + 2);
-		//TileUtils.createNoInteractionPlatform(roofTileLSmall, posX + 1, posY + 3);
+		TileUtils.createNoInteractionPlatform(doorEntrance, posX + 1, posY);
+		TileUtils.createNoInteractionPlatform(wallL, posX + 1, posY + 1);
+		TileUtils.createPlatform(wallLCorner, posX + 1, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileLSmall, posX + 1, posY + 3);
 		
-		//TileUtils.createNoInteractionPlatform(tableL, posX + 2, posY);
-		//TileUtils.createNoInteractionPlatform(windowL, posX + 2, posY + 1);
-		//TileUtils.createPlatform(wallTopLWindow, posX + 2, posY + 2);
-		//TileUtils.createNoInteractionPlatform(roofTileLLarge, posX + 2, posY + 3);
+		TileUtils.createNoInteractionPlatform(tableL, posX + 2, posY);
+		TileUtils.createNoInteractionPlatform(windowL, posX + 2, posY + 1);
+		TileUtils.createPlatform(wallTopLWindow, posX + 2, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileLLarge, posX + 2, posY + 3);
 		
-		//TileUtils.createNoInteractionPlatform(tableRCouchL, posX + 3, posY);
-		//TileUtils.createNoInteractionPlatform(windowR, posX + 3, posY + 1);
-		//TileUtils.createPlatform(wallTopRWindow, posX + 3, posY + 2);
-		//TileUtils.createNoInteractionPlatform(roofTileRLarge, posX + 3, posY + 3);
+		TileUtils.createNoInteractionPlatform(tableRCouchL, posX + 3, posY);
+		TileUtils.createNoInteractionPlatform(windowR, posX + 3, posY + 1);
+		TileUtils.createPlatform(wallTopRWindow, posX + 3, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileRLarge, posX + 3, posY + 3);
 		
-		//TileUtils.createNoInteractionPlatform(opemEntranceCouchR, posX + 4, posY);
-		//TileUtils.createNoInteractionPlatform(wallR, posX + 4, posY + 1);
-		//TileUtils.createPlatform(wallRCorner, posX + 4, posY + 2);
-		//TileUtils.createNoInteractionPlatform(roofTileRSmall, posX + 4, posY + 3);
-		
+		TileUtils.createNoInteractionPlatform(opemEntranceCouchR, posX + 4, posY);
+		TileUtils.createNoInteractionPlatform(wallR, posX + 4, posY + 1);
+		TileUtils.createPlatform(wallRCorner, posX + 4, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileRSmall, posX + 4, posY + 3);
+		*/
 		
 	}
-	else if(segmentNumber == 2)
+	else if(version == 2)
 	{
 		/*
 		TileUtils.createNoInteractionPlatform(openEntranceL, posX, posY);
@@ -65,36 +66,33 @@ public class RomanHouse {
 		
 		
 	}
-	else if(segmentNumber == 3)
+	else if(version == 3)
 	{
 		/*
-		TileUtils.createNoInteractionPlatform(TILENAME, posX, posY);
-		TileUtils.createPlatform(TILENAME, posX, posY + 1);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX, posY + 2);
+		TileUtils.createNoInteractionPlatform(columnL, posX, posY);
+		TileUtils.createPlatform(columnLTop, posX, posY + 1);
+		TileUtils.createNoInteractionPlatform(roolTileLSmall, posX, posY + 2);
 
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 1, posY);
-		TileUtils.createPlatform(TILENAME, posX + 1, posY + 1);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 1, posY + 2);
+		TileUtils.createPlatform(wallTop, posX + 1, posY + 1);
+		TileUtils.createNoInteractionPlatform(roolTileLLarge, posX + 1, posY + 2);
 
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 2, posY;
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 2, posY + 1);
-		TileUtils.createPlatform(TILENAME, posX + 2, posY + 2);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 2, posY + 3);
+		TileUtils.createNoInteractionPlatform(openEnteranceL, posX + 2, posY;
+		TileUtils.createNoInteractionPlatform(wallL, posX + 2, posY + 1);
+		TileUtils.createPlatform(wallLCorner, posX + 2, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileLSmall, posX + 2, posY + 3);
 
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 3, posY);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 3, posY + 1);
-		TileUtils.createPlatform(TILENAME, posX + 3, posY + 2);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 3, posY + 3);
+		TileUtils.createNoInteractionPlatform(windowL, posX + 3, posY + 1);
+		TileUtils.createPlatform(wallTopLWindow, posX + 3, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileLLarge, posX + 3, posY + 3);
 
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 4, posY);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 4, posY + 1);
-		TileUtils.createPlatform(TILENAME, posX + 4, posY + 2);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 4, posY + 3);
+		TileUtils.createNoInteractionPlatform(windowR, posX + 4, posY + 1);
+		TileUtils.createPlatform(wallTopRWindow, posX + 4, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileRLarge, posX + 4, posY + 3);
 
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 5, posY);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 5, posY + 1);
-		TileUtils.createPlatform(TILENAME, posX + 5, posY + 2);
-		TileUtils.createNoInteractionPlatform(TILENAME, posX + 5, posY + 3);
+		TileUtils.createNoInteractionPlatform(wallR, posX + 5, posY);
+		TileUtils.createNoInteractionPlatform(openEnteranceR, posX + 5, posY + 1);
+		TileUtils.createPlatform(wallRCorner, posX + 5, posY + 2);
+		TileUtils.createNoInteractionPlatform(roofTileRSmall, posX + 5, posY + 3);
 		*/
 		
 	}
